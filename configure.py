f93f998371f5c3d9b2bd09f86f56166d50da4054
hn3parmar <mm.2004.parmar@gmail.com>
2023-06-10 19:34:52 +0530

    Kongknitec is updating

---
+++ configure.py
@@ -0,0 +1,16 @@
+# Date    : 26/08/22 7:50 pm
+# Author  : Parmar Maulik (mm.2004.parmar@gmail.com)
+# GitHub    : (https://github.com/Maulikatgit)
+# Twitter    : (https://twitter.com/Mr_younglord)
+# Version : 1.0.0
+
+light_cyan = '#64FFDA'
+vivid_cyan = '#1de9b6'
+dark_gray = '#212121'
+white = '#ffffff'
+very_dark_gray = '#1a1a1a'
+screen_width = 0
+screen_height = 0
+font = 'Ubuntu Mono'
+welcome_fontstyle = (font, 19, "bold")
+header_fontstyle = (font, 25, "bold")
---
+++ main.py
@@ -0,0 +1,13 @@
+# Date    : 20/07/22 10:01 PM
+# Author  : Parmar Maulik (mm.2004.parmar@gmail.com)
+# GitHub    : (https://github.com/Maulikatgit)
+# Twitter    : (https://twitter.com/Mr_younglord)
+# Version : 1.0.0
+import customtkinter as ctk
+from Screens.stackpile import StackPile
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("blue")
+# initialize the root window
+app = StackPile()
+# start the application
+app.mainloop()
